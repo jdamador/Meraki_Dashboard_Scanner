ef16be99bef256a3c21fd4db660a10bbe90c3c6c
jdamador <amadorsalasjd@gmail.com>
2019-03-22 14:44:23 -0600

    chore: finish database functions

--- Meraki Databases/datameraki.sql
+++ Meraki Databases/datameraki.sql
@@ -37,8 +37,8 @@ CReate table observation(
 	constraint 	fk_observation_devices 	Foreign key(id_device) references devices(id_device),
 	constraint 	fk_observation_client 	Foreign key(id_client) references clients(id_client)
 );
+
 -- Table that contents all information about applications accessed.
--- 'activeTime': 660, 'numClients': 3}
 create table application (
 	id_application 	SERIAL 		not 	null,
 	application 	VARCHAR(200) 	NOT 	NULL,
@@ -52,6 +52,7 @@ create table application (
 	numClients 	int 		not 	null,
 	constraint 	pk_application 	primary key(id_application)
 );
+
 -- Table that contents all information about clients.
 create Table clients(
 	id_client 		SERIAL 		NOT 	NULL,
@@ -62,7 +63,6 @@ create Table clients(
 	mac 			varchar(50) 	not 	null 	unique,
 	ip 			varchar(20) 	not	null,
 	vlan 			Varchar(20) 	not	null,
-	switchport 		int 		not 	null,
 	first_seen 		iNT 		not 	null,
 	last_seen 		int 		not	null,
 	manufacturer 		varchar(100) 	not 	null,
@@ -71,26 +71,140 @@ create Table clients(
 	constraint 		pk_clients 	primary key(id_client)
 );
 -- Table that contents all information about clients usage.
+
 create table client_usage (
-	id_read 	SERIAL 		not 	null,
-	send 		int 		not	null,
-	rev 		int		not 	null,
-	reading_data 	timestamp 	not 	null,
-	constraint 	pk_client_usage primary key(id_read)
+	id_read 	SERIAL 			not 	null,
+	id_client 	INT 			NOT 	NULL,
+	send 		DOUBLE PRECISION 	not	null,
+	rev 		DOUBLE PRECISION	not 	null,
+	reading_data 	timestamp 		not 	null,
+	constraint 	pk_client_usage 	primary key(id_read),
+	constraint 	fk_client_usage 	FOREIGN kEy(id_client) references clients(id_client)
 );
-CREATE or replace FUNCTION insert_client(_id varchar, _des varchar, _mdns varchar, _dhcp varchar, _mac varchar, _ip varchar,_vlan varchar, _switch varchar, 
-_firstSeen INT,_lastSeen INT,_manufac VArCHAR, _os varchar, _wireless varchar)
-RETURNS BOOLEAN
+
+-- Function to add new clients.
+CREATE or replace FUNCTION insert_client(
+	_id varchar, 
+	_des varchar, 
+	_mdns varchar, 
+	_dhcp varchar, 
+	_mac varchar, 
+	_ip varchar,
+	_vlan varchar, 
+	_firstSeen INT,
+	_lastSeen INT,
+	_manufac VArCHAR,
+	_os varchar, 
+	_wireless varchar)
+RETURNS void
 AS $$
 BEGIN
-	if (select count(*) from clients where identifier = _id) > 0
-	then
-		UPDATE clients set last_seen = _lastSeen WHERE identifier = _id;
-	ELSE
-		insert into clients(identifier, description, mdnsName, dhcpHostname, mac,ip,vlan, switchport, first_seen, last_seen, manufacturer, os, wirelessCapabilities)
-		VALUES (_id, _des, _mdns, _dhcp, _mac, _ip, _vlan, _switch, _firstSeen, _lastSeen, _manufac, _os, _wireless);
-	end if;	
-	return true;
-END
+	-- IF the client exist, only update the last seen time.
+	if (select count(*) from clients where identifier = _id) > 0 THEN
+		Update clients set last_seen = __lastSeen where identifier = _id;
+	-- IF the client doesn't exist, add a new client.
+	else
+		Insert into clients(
+				identifier,description, mdnsName, dhcpHostname, mac, ip, vlan,
+				first_seen,last_seen, manufacturer, os, wirelessCapabilities)
+			Values(
+				_id, _des, _mdns, _dhcp, _mac, _ip, _vlan, _firstSeen, _lastSeen,
+				_manufac, _os, _wireless);
+	END IF;
+END;
 $$
 LANGUAGE plpgsql;
+
+-- Function that insert new application traffic register
+create or replace function insert_app(
+	_app VARCHAR,
+	_des Varchar,
+	_prot VARCHAR,
+	_port int,
+	_recv int,
+	_sent int,
+	_flows int,
+	_activeT int,
+	_numC INT)
+returns void
+as $$
+begin 
+	insert into application (
+		application, destination, protocol, port, recv, sent, flows, activeTime, numClients)
+		VALUES(_app, _des, _prot, _port, _recv, _sent, _flows, _activeT, _numC);
+end;
+$$
+Language plpgsql;
+
+-- Function that insert new usage readings.
+create or replace function insert_usage(
+	_mac VARCHAR,
+	_sent DOUBLE PRECISION,
+	_recv DOUBLE PRECISION,
+	_date TIMESTAMP)
+returns void
+as $$
+begin 
+	IF ( select count(*) from clients where mac = _mac) > 0 THEN
+		insert into client_usage( id_client, send, rev, reading_data) 
+		VALUES((SELECT id_client from clients where mac = _mac), _sent, _recv, _date);
+	end if;
+end
+$$
+Language plpgsql;
+
+
+/*
+id_divice 	SERIAL 		NOt 	NULL,
+	appMac  	Varchar(100) 	not 	null,
+	apFloor 	INT 		NOT 	NULL,
+	apTag 	
+	*/
+-- Function that insert a new device.
+create or replace function insert_device(
+	_appMac VARCHAR,
+	_appFloor INT,
+	_apTag INT)
+returns void
+as $$
+begin 
+	IF ( select count(*) from devices where appMac = _appMac) = 0 THEN
+		insert into devices( appMac, appFloor, apTag) 
+		VALUES(_appMac, _appFloor, _apTag);
+	end if;
+end
+$$
+Language plpgsql;
+
+--Function that insert a new Observation.
+create or replace function insert_observation(
+	_lat double precision,
+	_lng double precision,
+	_unc double precision,
+	_x double precision,
+	_y double precision,
+	_seenTime BIGINT,
+	_ssid Varchar,
+	_os Varchar,
+	_seenEpoch BIGINT,
+	_rssi INT,
+	_ipv6 Varchar,
+	_manufacturer Varchar,
+	_mac VARCHAR,
+	_macDevice VARCHAR
+	)
+returns void
+as $$
+begin 
+	if (select count(*) from devices where appMac = _macDevice) > 0 THEN
+		if (select count(*) from clients where mac = _mac) > 0 THEN
+			Insert into observations(
+				lng, unc, x, y, seenTime, ssid, os, seenEpoch, rssi, ipv6, manufacturer,id_client, id_devices)
+			VALUES(_lng, _unc, _x, _y, _seenTime, _ssid, _os, _seenEpoch, _rssi, _ipv6,
+			       _manufacturer, (select id_device from devices where appMac = _macDevice),
+			       (select id_client from clients where mac = _mac));
+		end if;
+	end if;
+end;
+$$
+Language plpgsql;
--- meraki_api_0.0.1.py
+++ meraki_api_0.0.1.py
@@ -14,54 +14,84 @@ from meraki import meraki
 # Import PostgreSQL Library.
 import psycopg2
 
+# System imports.
+import datetime
+
 # Simple routine to run a query on a database
 def insert(storageProcedure, values):
     # Data Base Configuration
-    connection = psycopg2.connect("dbname='datameraki' user='panelesfv' host='ec2-18-144-27-14.us-west-1.compute.amazonaws.com' password='P@n3L3sF0v0' port='9876'")
+    connection = psycopg2.connect(
+        "dbname='datameraki' user='panelesfv' host='ec2-18-144-27-14.us-west-1.compute.amazonaws.com' password='P@n3L3sF0v0' port='9876'")
     query = connection.cursor()
 
     # Set query structure
-    statement = 'SELECT * FROM ' + storageProcedure +'(' + values + ');'
+    statement = 'SELECT * FROM ' + storageProcedure + '(' + values + ');'
     print(statement)
     # Run query
     query.execute(statement)
     connection.commit()
     return
 
+
 # API Key to conecte with Meraki authentification services.
 API_KEY = "511c40271fbead284e859697874724ff2060a337"
 
 # Information about local divice MR33
-ORG_ID = 848678 # Organitation id.
+ORG_ID = 848678  # Organitation id.
 NETWORK_ID = 'N_609674799555303190'  # Network id.
-SERIAL_NUM = 'Q2PD-N83P-F9DM' # Device serial num.
+SERIAL_NUM = 'Q2PD-N83P-F9DM'  # Device serial num.
 
-''' Get all clients and their network usage. '''
-#query = "'%s','%s','%s','%s',%s,%s" % (i['id'],i['description'],i['mac'],i['ip'],i['usage']['sent'],i['usage']['recv']
+### GET clients information ###
+
+
+def get_clients():
+    clientsInfomation = meraki.getclients(API_KEY, SERIAL_NUM)
+    for index in clientsInfomation:
+        # GET Information about an specifit client.
+        client = meraki.getclient(API_KEY, NETWORK_ID, index['id'])
+        query = "'%s', '%s','%s', '%s', '%s', '%s','%s', %s, %s, '%s','%s', '%s'" % (
+            index['id'],  # VARCHAR
+            index['description'],  # VARCHAR
+            index['mdnsName'],  # VARCHAR
+            index['dhcpHostname'],  # VARCHAR
+            index['mac'],   # VARCHAR
+            index['ip'],  # VARCHAR
+            index['vlan'],  # VARCHAR
+            client['firstSeen'],  # INT
+            client['lastSeen'],  # INT
+            client['manufacturer'],  # VARCHAR
+            client['os'],  # VARCHAR
+            client['wirelessCapabilities'])  # VARCHAR
+
+        insert('insert_client', query)
 
 ### GET applications traffic stats ###
 
-appTraffic = meraki.getnetworktrafficstats(API_KEY, NETWORK_ID)
 
-### GET clients information ###
-clientsInfomation = meraki.getclients(API_KEY,SERIAL_NUM)
-for index in clientsInfomation: 
-  ### GET Information about an specifit client.
-  client = meraki.getclient(API_KEY,NETWORK_ID,index['id'])
-  query = "'%s', '%s','%s', '%s', '%s', '%s','%s', %s, %s, '%s','%s', '%s'" % (
-    index['id'],  # VARCHAR
-    index['description'], # VARCHAR
-    index['mdnsName'],  # VARCHAR
-    index['dhcpHostname'], # VARCHAR
-    index['mac'],   # VARCHAR
-    index['ip'], # VARCHAR
-    index['vlan'], # VARCHAR
-    client['firstSeen'], # INT
-    client['lastSeen'], # INT
-    client['manufacturer'], # VARCHAR
-    client['os'], #VARCHAR
-    client['wirelessCapabilities']) #VARCHAR
-    
-  insert('insert_client',query)
-  break
-  
+def get_traffic():
+    # application, destination, protocol, port, recv, sent, flows, activeTime, numClients
+    appTraffic = meraki.getnetworktrafficstats(API_KEY, NETWORK_ID)
+    for i in appTraffic:
+        query = "'%s', '%s', '%s', %s, %s, %s, %s, %s, %s" % (
+            i['application'],  # VARCHAR
+            i['destination'],  # VARCHAR
+            i['protocol'],  # VARCHAR
+            i['port'],  # INT
+            i['recv'],  # INT
+            i['sent'],  # INT
+            i['flows'],  # INT
+            i['activeTime'],  # INT
+            i['numClients'])  # INT
+        insert('insert_app',query)
+        break
+
+def get_clients_usage():
+  clients = meraki.getclients(API_KEY, SERIAL_NUM)
+  for index in clients:
+    query = "'%s', %s, %s, '%s'" %(
+      index['mac'],
+      index['usage']['sent'], 
+      index['usage']['recv'], 
+      datetime.datetime.now().date())
+    insert('insert_usage',query)
+    break
