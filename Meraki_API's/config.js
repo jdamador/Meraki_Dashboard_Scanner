21e407d40225923b824bef4a7cecfc92d83ef0bf
Jose Pablo Brenes Alfaro <josepablo1997@hotmail.com>
2019-03-25 13:50:24 -0600

    feat: config postgre

---
+++ Meraki_API's/config.js
@@ -0,0 +1,16 @@
+// ===================================
+// DB Configuration
+// ===================================
+
+const config = {
+    host: 'ec2-18-144-27-14.us-west-1.compute.amazonaws.com',
+    user: 'panelesfv',     
+    password: 'P@n3L3sF0v0',
+    database: 'datapaneles',
+    port: 9876,
+    ssl: true
+};
+
+module.exports = {
+   config 
+}
